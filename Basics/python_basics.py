8cdad41cf275fbcfce273e0549bdfe9f8af09a3e
Emir Yesilli <emir.yesilli@outlook.com>
2025-11-21 21:16:15 +0300

    Basics

--- Basics/python_basics.py
+++ Basics/python_basics.py
@@ -5,7 +5,7 @@
 age = 18
 
 #String
-name = "Emir"
+name = "emir yeşilli"
 
 #Bool
 ok = True
@@ -30,4 +30,23 @@ print(age%3)
 #Exponential expression
 print(age**2)
 
+#-------------------------Strings-----------------------------
+
+print(name.capitalize())
+print(name.find("i"))
+print(name.lower())
+print(name.upper())
+print(name.title())
+print(name.replace("i","j"))
+print(name.count("i"))
+print(name.center(40))
+print(name[0])
+print(len(name))
+print("emir" in name)
+print(name[:4])
+print(name[4:])
+print(name[-3:-1])
+print(name.encode())
+
 #------------------------------------------------------
+
