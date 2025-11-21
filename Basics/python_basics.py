b87a7ad30ec8a8620604b1a529b2c5ab2d237b5e
Emir Yesilli <emir.yesilli@outlook.com>
2025-11-21 20:35:33 +0300

    Basics

---
+++ Basics/python_basics.py
@@ -0,0 +1,33 @@
+
+#---------------------Variables---------------------
+
+#Integer
+age = 18
+
+#String
+name = "Emir"
+
+#Bool
+ok = True
+
+#Float
+pi = 3.14
+
+
+print(type(age))
+print(type(name))
+print(type(ok))
+print(type(pi))
+print(age + 5)
+print(age/3)
+
+#Only integer value
+print(age//4)
+
+#Remainder
+print(age%3)
+
+#Exponential expression
+print(age**2)
+
+#------------------------------------------------------
